d8ff6db228d5b37c81101ba0bfe0ed856d439730
josejesusgarciajr <josejesusgarciajr@gmail.com>
2022-01-03 15:11:41 -0800

    Healing is not Linear. You are Healing. You Will Heal!

---
+++ Healing/Healing/Models/Note.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Healing.Models
+{
+    public class Note
+    {
+        /*
+         * How You are Feeling
+         * These are the bad symptoms
+         */ 
+        public Double Anxiety { get; set; }
+        public Double Uneasyness { get; set; }
+        public Double Heavyness { get; set; }
+
+        /*
+         * Happy Feelings 
+         */
+        public Double Happyness { get; set; }
+        public Double Excitement { get; set; }
+
+        /*
+         * Expression:
+         *  Express how you are feeling
+         *  Is it better or worse than the previous day?
+         *  Any Thoughts running through your mind?
+         *  
+         *  Remember Healing is not linear
+         *  Be kind to Yourself
+         */
+        public String Expression { get; set; }
+
+        public Note() { }
+
+        public Note(double anxiety, double uneasyness, double heavyness,
+            double happyness, double excitement, string expression)
+        {
+            Anxiety = anxiety;
+            Uneasyness = uneasyness;
+            Heavyness = heavyness;
+            Happyness = happyness;
+            Excitement = excitement;
+            Expression = expression;
+        }
+    }
+}
