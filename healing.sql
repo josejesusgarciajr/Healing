940fcc0fc828dbc3653bf8e95fee7207c1b7d8f7
josejesusgarciajr <josejesusgarciajr@gmail.com>
2022-01-05 12:56:52 -0800

    Inserted Past Notes

--- healing.sql
+++ healing.sql
@@ -20,3 +20,12 @@ INSERT INTO Note
 VALUES
 ('Sunday, January 2, 2022 6:25 PM', 2, 1, 10, 75, 95, 'I got a spark at work :) It felt good to feel excited'),
 ('Sunday, January 2, 2022 10:25 PM', 0, 1, 5, 95, 95, 'I had an amazing time with my girlfriend. It felt like before. We danced, we took pictures, we went for a walk. I was happy :)');
+
+INSERT INTO Note
+VALUES
+('Monday, January 3, 2022 7:00 PM', 0, 1, 5, 90, 95, 'Felt good. We ran at Santa Monica. We were able to get on the pier! We walked around. Saw the Lights, and even ate hot dogs which was the highlight of the night :)');
+
+
+INSERT INTO Note
+VALUES
+('Wednesday, January 5, 2022 12:55 PM', 2, 3, 30, 45, 25, 'Woke up feeling alright. The heavyness is not so bad. Baby steps. You are going to make it a good day.');
